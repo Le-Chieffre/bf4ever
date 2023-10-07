4c40ab80d8c0ffd6bd2762ed3459a3952da39bb8
gustavjohansen98 <gustavlj@live.dk>
2023-10-07 21:44:32 +0200

    ccreated scraper

--- src/app.py
+++ src/app.py
@@ -1,22 +1,18 @@
-import pandas as pd
 import streamlit as st
 
+# 💸💦🐐
+
 """
-# Welcome to Streamlit!
+## 💦 Welcome to Billionaire's Dream 💸
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
+- hello
+- _test_
 """
 
+input_result = st.text_input(
+    label="Input URL", 
+    value="example.com",
+)
 
-with st.echo(code_location='below'):        
-    df = pd.DataFrame({
-        'first column': [1, 2, 3, 4],
-        'second column': [10, 20, 30, 40]
-    })
-
-    df
+if input_result:
+    input_result
